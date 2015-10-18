03b78fd858ce8dde32e11313af1a9604020d3c76
Rachel Bobbins <rbobbins@stitchfix.com>
2015-10-17 22:49:27 -0700

    Added failing test to confirm Quick setup is OK

---
+++ EarthquakeCounterTests/EarthquakeViewControllerSpec.swift
@@ -0,0 +1,11 @@
+import Quick
+import Nimble
+import EarthquakeCounter
+
+class EarthquakeViewControllerSpec: QuickSpec {
+    override func spec() {
+        it("should run the tests") {
+            expect(1 + 1).to(equal(3))
+        }
+    }
+}
